10d3aceef5de547aae095679c0ddef86626d12d5
Rebecca Weiss <71280177+rebecca-weiss@users.noreply.github.com>
2022-03-29 22:06:31 -0400

    Delete runPlunk directory

--- runPlunk/getExamples.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# getExamples.sh
-
-
-# Get the example data
-wget http://zzz.bwh.harvard.edu/plink/hapmap1.zip
-
-# Unzip the datafile
-unzip hapmap1.zip
--- runPlunk/render.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# render.sh
-
-R -e "rmarkdown::render('runPlink.Rmd', output_format='all')"
